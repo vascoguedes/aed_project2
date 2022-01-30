4f969ff82e83694f703d3229f6d413107d235da4
Joao Araujo <joaomatos7602@gmail.com>
2022-01-30 23:04:49 +0000

    Graph documentation

--- code/graph.h
+++ code/graph.h
@@ -43,25 +43,92 @@ class Graph {
          * @brief Initializer
          */
         Graph();
+        /**
+        * @brief  closestPath between 2 places
+        * @return verify the existence of path between 2 places
+        */
         bool closestPath(int src, int dest, int criteria, bool daytimeSchedule);
+
+        /**
+        * @brief search algorhitm
+        * @return if exists an element in the graph
+        */
         bool bfs(int src, int dest, bool daytimeSchedule);
+
+        /**
+        * @brief algorhitm that calculates the shortest paht by the cost of edges
+        * @return if exists that path
+        */
         bool dijkstra(int src, int dest, int criteria, bool daytimeSchedule, string initialLine = "walking-line");
 
+        /**
+        * @brief show the closest path
+        */
         void printClosestPath(int dest);
+        /**
+        * @brief show possible paths once, we have a destination
+        */
         void printPossiblePath(int dest);
+
+        /**
+        * @brief process to load the station of stcp and read the files
+        */
         void loadStations ();
+        /**
+        * @brief process to load the lines of stcp and read the files
+        */
         void loadLines ();
+
+        /**
+        * @brief close or open one line
+        */
         void closeOpenLine(const string& line, bool closeOpen);
+
+        /**
+        * @brief close or open one station
+        */
         void closeOpenStation(int index, bool closeOpen);
+        /**
+        * @brief create the possible path to walk
+        */
         void createWalking(int maxWalking);
+        /**
+        * @brief remove the path to walk
+        */
         void rmvWalking();
 
+        /**
+        * @brief price of ticket
+        * @return the price of ticket
+        */
+
         float ticketPrice(int numZones);
+        /**
+        * @brief time of ticket's validation
+        * @return the validate time of ticket
+        */
         float ticketTime(int numZones);
 
+         /**
+        * @brief near by stations
+        * @return the stations near by
+        */
         vector<pair<int, double>> closestStation(double latitude, double longitude, int num) const;
+        /**
+        * @brief Distance of the station giving coordenades
+        * @return the distance to the station
+        */
         double stationDistance(int src, int dest) const;
+
+        /**
+        * @brief Distance of the station giving a station
+        * @return the distance to the station
+        */
         double stationDistance(int station, double lat2, double lon2) const;
+        /**
+        * @brief To see if the client has to change to another zone
+        * @return the possiblity of change the zone
+        */
         bool changeZone(int src, int dest);
 };
 
